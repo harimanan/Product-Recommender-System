9f636d88b5c45cfb183ab649487d9a3e42a749f8
harimanan <hari.manan@gmail.com>
2017-12-25 22:14:42 -0500

    Add files via upload

---
+++ payment/signals.py
@@ -0,0 +1,30 @@
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from paypal.standard.models import ST_PP_COMPLETED
+from paypal.standard.ipn.signals import valid_ipn_received
+#import weasyprint
+from io import BytesIO
+from orders.models import Order
+
+
+def payment_notification(sender, **kwargs):
+    ipn_obj = sender
+    if ipn_obj.payment_status == ST_PP_COMPLETED:
+        # payment was successful
+        order = get_object_or_404(Order, id=ipn_obj.invoice)
+
+        # mark the order as paid
+        order.paid = True
+        order.save()
+
+        # create invoice e-mail
+        subject = 'My Shop - Invoice nr. {}'.format(order.id)
+        message = 'Please, find attached the invoice for your recent purchase.'
+        email = EmailMessage(subject, message, 'admin@myshop.com', [order.email])
+
+        # send e-mail
+        email.send()
+
+valid_ipn_received.connect(payment_notification)
